84fe6c85b25229d33185bd4588372c1f1d5f0b2d
Delaney <stimsoni9@gmail.com>
2016-08-12 13:02:58 +1000

    Give DbDataLoader the ability to listen to multiple urn's

--- stonecraft-orm/src/main/java/com/stonecraft/datastore/DbDataLoader.java
+++ stonecraft-orm/src/main/java/com/stonecraft/datastore/DbDataLoader.java
@@ -1,16 +1,19 @@
 package com.stonecraft.datastore;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.os.CancellationSignal;
 import android.support.v4.os.OperationCanceledException;
-import android.text.TextUtils;
 
 import com.stonecraft.datastore.exceptions.DatabaseException;
 import com.stonecraft.datastore.interaction.Query;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by michaeldelaney on 18/11/15.
@@ -18,10 +21,9 @@ import java.util.Calendar;
 public class DbDataLoader<T> extends AsyncTaskLoader<T> {
 
     private volatile CancellationSignal myCancellationSignal;
-    private final ForceLoadContentObserver myObserver;
+    private Map<Uri, ForceLoadContentObserver> myObservers;
     private T myResult;
     private final String myDbName;
-    private Uri myAlternateWatchUri;
     private final Query myQuery;
     private final Class myLoaderResultType;
     private Calendar myTableUpdateTime;
@@ -29,7 +31,9 @@ public class DbDataLoader<T> extends AsyncTaskLoader<T> {
 
     public DbDataLoader(Context context, String dbName, Query query, Class loaderResultType) {
         super(context);
-        myObserver = new ForceLoadContentObserver();
+        myObservers = new HashMap<>();
+        Datastore ds = Datastore.getDataStore(dbName);
+        addWatchUri(ds.getTableUri(query.getTable()));
         myDbName = dbName;
         myQuery = query;
         myLoaderResultType = loaderResultType;
@@ -87,26 +91,63 @@ public class DbDataLoader<T> extends AsyncTaskLoader<T> {
 
     public void ignoreUpdates(){
         myIsIgnoringUpdates = true;
-        getContext().getContentResolver().unregisterContentObserver(myObserver);
+        unregisterContentObservers();
+
     }
 
     public void receiveUpdates(){
         myIsIgnoringUpdates = false;
-        Uri tableUri = getWatchUri();
-        if(isStarted() && tableUri != null && !TextUtils.isEmpty(tableUri.toString())) {
-            getContext().getContentResolver().registerContentObserver(getWatchUri(), false, myObserver);
+        if(isStarted()) {
+            ContentResolver cr = getContext().getContentResolver();
+            for(Map.Entry<Uri, ForceLoadContentObserver> entry : myObservers.entrySet()) {
+                cr.registerContentObserver(entry.getKey(), false, entry.getValue());
+            }
         }
     }
 
-    public void setAlternateWatchUri(Uri uri){
-        myAlternateWatchUri = uri;
+    /**
+     * This method adds a uri that this loader will receive updates and restart should any changes
+     * occur.
+     *
+     * By default this loader will listen to changes to the query this loader is based on. If this
+     * loader should not listen to these changes removeWatchUri should be called with the uri
+     * that matches the main tables uri found in the query that was passed in when this class was
+     * instantiated
+     *
+     * @param uri
+     */
+    public void addWatchUri(@Nullable Uri uri){
+        if(uri != null && !myObservers.containsKey(uri)){
+            ForceLoadContentObserver observer = new ForceLoadContentObserver();
+            myObservers.put(uri, observer);
+            if(isStarted()) {
+                ContentResolver cr = getContext().getContentResolver();
+                cr.registerContentObserver(uri, false, observer);
+            }
+        }
+    }
+
+    /**
+     * This method removes the uri from this class so that it no longer hears changes that occur on
+     * this uri.
+     *
+     * @param uri
+     */
+    public void removeWatchUri(Uri uri){
+        if(myObservers.containsKey(uri)){
+            if(isStarted()) {
+                ContentResolver cr = getContext().getContentResolver();
+                cr.unregisterContentObserver(myObservers.get(uri));
+            }
+
+            myObservers.remove(uri);
+        }
     }
 
     @Override
     protected void onStartLoading() {
-        Uri tableUri = getWatchUri();
-        if(!myIsIgnoringUpdates && tableUri != null && !TextUtils.isEmpty(tableUri.toString())) {
-            getContext().getContentResolver().registerContentObserver(tableUri, false, myObserver);
+        if(!myIsIgnoringUpdates) {
+            receiveUpdates();
         }
 
         Datastore ds = Datastore.getDataStore(myDbName);
@@ -131,7 +172,7 @@ public class DbDataLoader<T> extends AsyncTaskLoader<T> {
     protected void onStopLoading() {
         // Attempt to cancel the current load task if possible.
         cancelLoad();
-        getContext().getContentResolver().unregisterContentObserver(myObserver);
+        unregisterContentObservers();
     }
 
     @Override
@@ -142,13 +183,10 @@ public class DbDataLoader<T> extends AsyncTaskLoader<T> {
         onStopLoading();
     }
 
-    private Uri getWatchUri(){
-        if(myAlternateWatchUri != null){
-            return myAlternateWatchUri;
+    private void unregisterContentObservers() {
+        ContentResolver cr = getContext().getContentResolver();
+        for(ForceLoadContentObserver observer : myObservers.values()) {
+            cr.unregisterContentObserver(observer);
         }
-
-        Datastore ds = Datastore.getDataStore(myDbName);
-        return ds.getTableUri(
-                myQuery.getTable());
     }
 }
